53c043ad8ef0a7bc9f1a93ce6416b069af3f2141
Konrad Jurkowski <konrad@jurkowski.me>
2015-01-02 23:21:46 +0100

    Added male nicknames, female names and nicknames to FantasyFaker::Name

--- lib/fantasy_faker/name.rb
+++ lib/fantasy_faker/name.rb
@@ -10,6 +10,26 @@ module FantasyFaker
       "%s%s" % [NAME_PREFIXES_MALE.rand, NAME_SUFIXES_MALE.rand]
     end
 
+    def male_nickname
+      NICKNAMES_MALE.rand
+    end
+
+    def male_full_name
+      "%s %s" % [male_name, male_nickname]
+    end
+
+    def female_name
+      "%s%s" % [NAME_PREFIXES_FEMALE.rand, NAME_SUFIXES_FEMALE.rand]
+    end
+
+    def female_nickname
+      NICKNAMES_FEMALE.rand
+    end
+
+    def female_full_name
+      "%s %s" % [female_name, female_nickname]
+    end
+
   end
 
 end
